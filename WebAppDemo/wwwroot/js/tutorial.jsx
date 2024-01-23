63ce5e5cd3d71d575936f5ab0c6eda3788f8d2ac
carsonkrueger <crkruege@gmail.com>
2024-01-22 19:32:32 -0700

    Add project files.

---
+++ WebAppDemo/wwwroot/js/tutorial.jsx
@@ -0,0 +1,7 @@
+﻿const CommentBox = () => {
+    return (
+            <div className="commentBox">Hello, world! I am a CommentBox.</div>
+        );
+}
+
+ReactDOM.render(<CommentBox />, document.getElementById('content'));
